c6d43dd640c4b1ac2ec2c6d69b3636b342f59c76
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-11-23 21:14:56 +0900

    updated

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const todoInput = document.querySelector('.todo-input');
+const todoBtm = document.querySelector('.todo-button');
+
+
+todoBtm.addEventListener('click',addList);
+
+function addList(e){
+    e.preventDefault();
+    const list = document.createElement('div');
+    list.classList.add('.todo');
+
+    const newList = document.createElement('li');
+    newList.innerText = todoInput.value;
+    console.log(newList)
+}
